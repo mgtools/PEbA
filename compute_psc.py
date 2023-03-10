0c0d1e69601c075d278a0bc9a3cec9c05aeea5f0
ben <biovino@gmail.com>
2023-03-10 09:06:08 -0500

    this is tougher than expected

--- compute_psc.py
+++ compute_psc.py
@@ -42,6 +42,33 @@ def parse_align(filename, id1, id2):
     return seq1, seq2
 
 
+def match_pairs(align):
+    """=============================================================================================
+    This function accepts an alignment and returns a dictionary of the pairs that are matched.
+
+    :param aligns: dict of an alignment
+    return dict: pairs
+    ============================================================================================="""
+
+    seq1_count = 0
+    seq2_count = 0
+    pairs = {}
+    for i in range(len(align[0])):
+        seq1_char = align[0][i]
+        seq2_char = align[1][i]
+        if seq1_char == '.':
+            seq2_count += 1
+            continue
+        if seq2_char == '.':
+            seq1_count += 1
+            continue
+        pairs[f'{seq1_char}{seq1_count}'] = f'{seq2_char}{seq2_count}'
+        seq1_count += 1
+        seq2_count += 1
+    print(pairs)
+    return pairs
+
+
 def compute_score(aligns, beg, end):
     """=============================================================================================
     This function accepts two alignments and returns the PSC between them.
@@ -59,27 +86,33 @@ def compute_score(aligns, beg, end):
     if end == 0:
         end = min(len(align1[0]), len(align2[0]))
 
-    # Compare columns in specified range
-    shared_cols = 0
-    length = 0
-    similarity = 0
+    count1 = 0
+    count2 = 0
     for i in range(beg, end):
-        pair1 = (align1[0][i], align1[1][i])
-        pair2 = (align2[0][i], align2[1][i])
-        if '.' in pair1:  # Skip columns with gaps in reference alignment
+        ref_pair = (align1[0][i], align1[1][i])
+        test_pair = (align2[0][i], align2[1][i])
+        print(ref_pair[0], count1, test_pair[0], count2)
+        if ref_pair[0] == '.' and test_pair[0] == '.':
+            count1 += 1
+            count2 += 1
             continue
-        if pair1[0] == pair1[1]:  # Increment similarity if residues are identical
-            similarity += 1
-        if pair1 == pair2:
-            shared_cols += 1
-        length += 1  # Increment length of comparison
+        if '.' in ref_pair[0] or '.' in test_pair[0]:
+            if '.' in ref_pair:
+                count1 += 1
+            if '.' in test_pair:
+                count2 += 1
+        elif ref_pair[0] != '.' and test_pair[0] != '.':
+            count1 += 1
+            count2 += 1
+    print(count1)
+
 
     # PSC is shared_cols / length of comparison
-    psc = round(shared_cols / length, 4)
-    sim = round(similarity / length, 4)
-    logger.info('PCS: %s   ref_length: %s   comparison_length: %s   comparison_region: %s-%s   similarity: %s',
-                psc, len(align1[0]), length, beg, end, sim)
-    return psc
+    #psc = round(shared_cols / length, 4)
+    #sim = round(similarity / length, 4)
+    #logger.info('PCS: %s   ref_length: %s   comparison_length: %s   comparison_region: %s-%s   similarity: %s',
+                #psc, len(align1[0]), length, beg, end, sim)
+    #return psc
 
 
 def main():
@@ -104,3 +137,23 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""=============================================================================================
+    # Compare columns in specified range
+    shared_cols = 0
+    length = 0
+    similarity = 0
+    for i in range(beg, end):
+
+        # Define pairs of residues in each alignment
+        pair1 = (align1[0][i], align1[1][i])
+        pair2 = (align2[0][i], align2[1][i])
+        if '.' in pair1:  # Skip columns with gaps in reference alignment
+            continue
+        if pair1[0] == pair1[1]:  # Increment similarity if residues are identical
+            similarity += 1
+        if pair1 == pair2:
+            shared_cols += 1
+        length += 1  # Increment length of comparison
+    ============================================================================================="""
