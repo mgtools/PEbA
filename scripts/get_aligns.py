de438a6976534d4552ca957f3fd16ce4e022929f
ben <biovino@gmail.com>
2023-10-05 08:32:00 -0400

    check if aligns already exist

--- scripts/get_aligns.py
+++ scripts/get_aligns.py
@@ -6,10 +6,42 @@ __date__ = "09/22/23"
 """
 
 import argparse
+import datetime
 import os
+import logging
 #import tensorflow as tf
 import utility as ut
 
+log_filename = 'data/logs/get_aligns.log'  #pylint: disable=C0103
+os.makedirs(os.path.dirname(log_filename), exist_ok=True)
+logging.basicConfig(filename=log_filename, filemode='w',
+                     level=logging.INFO, format='%(message)s')
+
+
+def log_align(ref: str, direc: str, pair: str, method: str) -> bool:
+    """Returns True if alignment needs to be done, False if not
+
+    :param ref: reference folder
+    :param direc: subfolder
+    :param pair: pair of sequences
+    :param method: alignment method
+    :return bool: True or False
+    """
+
+    # Check if alignment already exists
+    seq1, seq2 = pair[0].split('.')[0], pair[1].split('.')[0]
+    if os.path.isfile(f'data/alignments/{method}/{ref}/{direc}/{seq1}-{seq2}.msf'):
+        logging.info('%s\t%s/%s/%s-%s %s',
+                      datetime.datetime.now(), ref, direc, seq1, seq2, 'already aligned')
+
+        return False
+    
+    # If not, need to align
+    logging.info('%s\t%s %s/%s/%s-%s',
+                  datetime.datetime.now(), 'aligning', ref, direc, seq1, seq2)
+
+    return True
+    
 
 def get_aligns(seqs):
     """Returns a list of all pairs in a list of sequences
@@ -48,6 +80,8 @@ def blosum(pw_aligns: list, ref: str, direc: str, method: str):
 
     # Align each pair of sequences
     for pair in pw_aligns:
+        if not log_align(ref, direc, pair, f'{method}_blosum'):  # Ignore if already aligned
+            continue
         seq1, seq2 = pair[0], pair[1]
 
         args = (f'--align {method} '
@@ -58,7 +92,7 @@ def blosum(pw_aligns: list, ref: str, direc: str, method: str):
                 f'--matrix blosum '
                 f'--score 62 '
                 f'--savefile {method_direc}/{ref}/{direc}')
-        os.system(f'python matrix.py {args}')
+        os.system(f'python scripts/matrix.py {args}')
 
 
 def peba(pw_aligns: list, ref: str, direc: str, method: str):
@@ -80,6 +114,8 @@ def peba(pw_aligns: list, ref: str, direc: str, method: str):
 
     # Align each pair of sequences
     for pair in pw_aligns:
+        if not log_align(ref, direc, pair, f'{method}_peba'):  # Ignore if already aligned
+            continue
         seq1, seq2 = pair[0], pair[1]
         embed1 = seq1.split('.')[0] + '.txt'
         embed2 = seq2.split('.')[0] + '.txt'
@@ -92,22 +128,21 @@ def peba(pw_aligns: list, ref: str, direc: str, method: str):
                 f'--gopen -11 '
                 f'--gext -1 '
                 f'--savefile {method_direc}/{ref}/{direc}')
-        os.system(f'python peba.py {args}')
+        os.system(f'python scripts/peba.py {args}')
 
 
-def dedal_run(pw_aligns: list, ref: str, direc: str, method: str, model):
+def dedal_run(pw_aligns: list, ref: str, direc: str, model):
     """Writes all pairwise dedal alignments to a file in the msf format
 
     :param pw_aligns: list of all pairwise combinations of sequences
     :param ref: reference folder
     :param direc: subfolder
-    :param method: alignment method
     :param model: dedal model
     """
 
     from dedal import infer  #pylint: disable-all
 
-    method_direc = f'data/alignments/{method}'
+    method_direc = 'data/alignments/dedal'
     if not os.path.isdir(method_direc):
         os.makedirs(method_direc)
     if not os.path.isdir(f'{method_direc}/{ref}'):
@@ -117,9 +152,12 @@ def dedal_run(pw_aligns: list, ref: str, direc: str, method: str, model):
 
     # Align each pair of sequences
     for pair in pw_aligns:
+        if not log_align(ref, direc, pair, 'dedal'):  # Ignore if already aligned
+            continue
         seq1, id1 = ut.parse_fasta(f'data/sequences/{ref}/{direc}/{pair[0]}')
         seq2, id2 = ut.parse_fasta(f'data/sequences/{ref}/{direc}/{pair[1]}')
 
+        # Get alignment from dedal
         inputs = infer.preprocess(seq1, seq2)
         align_out = model(inputs)
         output = infer.expand(
@@ -134,7 +172,7 @@ def dedal_run(pw_aligns: list, ref: str, direc: str, method: str, model):
         end = [align1.split()[2], align2.split()[2]]
         align1 = align1.split()[1].replace('-', '.')
         align2 = align2.split()[1].replace('-', '.')
-        ut.write_msf(align1, align2, id1, id2, method,
+        ut.write_msf(align1, align2, id1, id2, 'dedal',
                      0, 0, f'{method_direc}/{ref}/{direc}', beg, end)
         
 
@@ -162,6 +200,8 @@ def fatcat(pw_aligns: list, ref: str, direc: str):
 
     # Align each pair of sequences
     for pair in pw_aligns:
+        if not log_align(ref, direc, pair, 'fatcat'):  # Ignore if already aligned
+            continue
         seq1, seq2 = pair[0], pair[1]
         pdb1, pdb2 = seq1.split('.')[0], seq2.split('.')[0]
         args = (f'-f1 data/pdb/{direc}/{pdb1}.pdb '
@@ -184,6 +224,8 @@ def vcmsa(pw_aligns: list, ref: str, direc: str):
 
     # Align each pair of sequences
     for pair in pw_aligns:
+        if not log_align(ref, direc, pair, 'vcmsa'):  # Ignore if already aligned
+            continue
         seq1, seq2 = pair[0], pair[1]
         args = (f'-f1 data/sequences/{ref}/{direc}/{seq1} '
                 f'-f2 data/sequences/{ref}/{direc}/{seq2} '
@@ -196,8 +238,8 @@ def main():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', type=str, default='vcmsa')
-    parser.add_argument('-m', type=str, default='vcmsa')
+    parser.add_argument('-a', type=str, default='blosum')
+    parser.add_argument('-m', type=str, default='global')
     args = parser.parse_args()
 
     # Don't want to load if not using
@@ -223,7 +265,7 @@ def main():
             elif args.a == 'peba':
                 peba(pw_aligns, ref, direc, args.m)
             elif args.a == 'dedal':
-                dedal_run(pw_aligns, ref, direc, args.m, dedal_model)
+                dedal_run(pw_aligns, ref, direc, dedal_model)
             elif args.a == 'fatcat':
                 fatcat(pw_aligns, ref, direc)
             elif args.a == 'vcmsa':
